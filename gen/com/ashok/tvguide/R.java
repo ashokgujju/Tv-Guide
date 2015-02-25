d9534640b03aa19593689f4f870e1676f4a41783
ashokgujju <ashokg.geek@gmail.com>
2015-02-25 08:06:06 +0530

    Revert "Reduce screenshots size"
    This reverts commit 9bbaa61684414a46a7e1a3cb246e07d9f47be5f8.

--- gen/com/ashok/tvguide/R.java
+++ gen/com/ashok/tvguide/R.java
@@ -1071,7 +1071,10 @@ containing a value of this type.
         public static final int abs=0x7f020057;
         public static final int asdf=0x7f020058;
         public static final int ic_launcher=0x7f020059;
-        public static final int show=0x7f02005a;
+        public static final int screen1=0x7f02005a;
+        public static final int screen2=0x7f02005b;
+        public static final int screen3=0x7f02005c;
+        public static final int show=0x7f02005d;
     }
     public static final class id {
         public static final int action_bar=0x7f09001c;
